4b31f9753a849bb180aeb48449f381312f29e531
Stuart Blair <stuart.a.blair@gmail.com>
2016-03-14 23:36:19 -0700

    Fixing typo in template locations mentioned in the controller

--- src/main/java/com/github/stuartblair/HelloController.java
+++ src/main/java/com/github/stuartblair/HelloController.java
@@ -27,13 +27,13 @@ public class HelloController {
     @RequestMapping(method=RequestMethod.GET)
     public String helloTwitter(Model model) {
         if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
-            return "redirect:/connect/twitter";
+            return "redirect:connect/twitter";
         }
 
         model.addAttribute(twitter.userOperations().getUserProfile());
         CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
         model.addAttribute("friends", friends);
-        return "/connect/hello";
+        return "connect/hello";
 
     }
 }
